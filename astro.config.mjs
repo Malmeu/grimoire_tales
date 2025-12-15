7e3cee20b42419f8c3ec5ae5e4576c2adda3750a
Malmeu <darkvid3r@gmail.com>
2025-12-15 16:20:55 +0100

    Revert to static mode to fix Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind()
   ],
-  output: 'server',
-  adapter: vercel({
-    runtime: 'nodejs20.x'
-  }),
+  output: 'static',
   vite: {
     ssr: {
       noExternal: ['gsap']
