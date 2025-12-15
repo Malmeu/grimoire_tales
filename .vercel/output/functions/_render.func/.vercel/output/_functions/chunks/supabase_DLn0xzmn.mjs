b0b3ba2b4c059b676a2a225539dc3e82530f230e
Malmeu <darkvid3r@gmail.com>
2025-12-15 16:11:31 +0100

    Add .gitignore and remove build artifacts from repo

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/supabase_DLn0xzmn.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://ooevpxkcftpemofjantd.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vZXZweGtjZnRwZW1vZmphbnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3Njg3ODYsImV4cCI6MjA4MTM0NDc4Nn0.rbUanSpOKtwqcSs71eCLw_K2ywpuWSfG9Ae88hBmu6M";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase as s };
